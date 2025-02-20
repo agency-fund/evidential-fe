0112bbf25673d609a7c5c7334adc0761ab7da4fe
dctaf <155950283+dctaf@users.noreply.github.com>
2025-02-20 10:29:05 -0800

    orgprovider fixes

--- src/app/organization-provider.tsx
+++ src/app/organization-provider.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { OrganizationSummary } from '@/api/methods.schemas';
-import { createContext, PropsWithChildren, useContext } from 'react';
+import { createContext, PropsWithChildren, useContext, useEffect } from 'react';
 import { useListOrganizations } from '@/api/admin';
 import { useAuth } from './auth-provider';
 import { useLocalStorage } from '@/services/use-local-storage';
-import { Text, Spinner } from '@radix-ui/themes';
+import { Button, Flex, Spinner, Text } from '@radix-ui/themes';
+import { isHttpOk } from '@/services/typehelper';
 
 type OrganizationContext = {
   current: OrganizationSummary;
@@ -22,30 +23,59 @@ export function useCurrentOrganization() {
   return context;
 }
 
+const CURRENT_ORG_ID_KEY = 'org_id' as const;
+
 export function OrganizationProvider({ children }: PropsWithChildren) {
   const auth = useAuth();
-  const { data: orgsResponse, isLoading } = useListOrganizations();
-  const [orgId, setOrgId] = useLocalStorage<string>('org_id');
+  const {
+    data: orgsList,
+    isLoading,
+    error,
+  } = useListOrganizations({
+    swr: {
+      enabled: auth.isAuthenticated,
+    },
+  });
+  const [orgId, setOrgId] = useLocalStorage<string>(CURRENT_ORG_ID_KEY);
 
+  // If the localstorage org_id refers to an org that the user doesn't have access to, update it to be the first one
+  // that they do have access to.
+  useEffect(() => {
+    if (!isHttpOk(orgsList) || !orgsList.data.items.length) return;
+
+    const organizations = orgsList.data.items;
+    const storedOrgExists = organizations.some((org) => org.id === orgId);
+    if (storedOrgExists) return;
+    setOrgId(organizations[0].id);
+  }, [orgsList, orgId, setOrgId]);
   if (isLoading) {
-    return <Spinner />;
+    return (
+      <Flex direction="column" gap="3" align="center">
+        <Spinner />
+        <Text size="2">Loading organization information...</Text>
+      </Flex>
+    );
   }
-
-  if (!auth.isAuthenticated) {
+  if (error) {
+    console.error('Error loading organizations', error);
+    return (
+      <Flex direction="column" gap="3" align="center">
+        <Text>Unable to load your organization information.</Text>
+        <Button onClick={() => window.location.reload()}>Retry</Button>
+      </Flex>
+    );
+  }
+  if (!auth.isAuthenticated || !isHttpOk(orgsList)) {
     return <CurrentOrganizationContext.Provider value={null}>{children}</CurrentOrganizationContext.Provider>;
   }
 
-  const organizations = orgsResponse?.data.items ?? [];
+  const organizations = orgsList.data.items;
   if (organizations.length === 0) {
     return <Text>Sorry, you are not a member of any organizations.</Text>;
   }
 
   const currentOrgId = orgId ?? organizations[0].id;
-  const current = organizations.find((v) => v.id == currentOrgId);
-  if (current === undefined) {
-    setOrgId(null);
-    return <CurrentOrganizationContext.Provider value={null}>{children}</CurrentOrganizationContext.Provider>;
-  }
+  const current = organizations.find((v) => v.id == currentOrgId) || organizations[0];
   return (
     <CurrentOrganizationContext.Provider
       value={{
