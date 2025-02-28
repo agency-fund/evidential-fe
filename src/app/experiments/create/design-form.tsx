3e9f9850428ba9a31ee17b78cf07ee3e673c2ef4
dctaf <155950283+dctaf@users.noreply.github.com>
2025-02-27 22:28:44 -0800

    incremental progress on DesignSpec conversion

--- src/app/experiments/create/design-form.tsx
+++ src/app/experiments/create/design-form.tsx
@@ -54,13 +54,15 @@ const convertFormDataToCreateExperimentRequest = (formData: ExperimentFormData):
       end_date: formData.endDate,
       start_date: formData.startDate,
       description: formData.hypothesis,
-      metrics: [...formData.primaryMetric!, ...formData.secondaryMetrics].map(
+      metrics: [formData.primaryMetric!, ...formData.secondaryMetrics].map(
         (field_name): DesignSpecMetricRequest => ({
           field_name: field_name,
-          metric_pct_change: 1, // TODO
+          metric_pct_change: Number(formData.effectPctChange) / 100.0, // TODO: populate with effect % change
         }),
       ),
       strata_field_names: [],
+      power: Number(formData.power) / 100.0,
+      alpha: 1 - Number(formData.confidence) / 100.0,
       experiment_id: null,
     },
     audience_spec: {
