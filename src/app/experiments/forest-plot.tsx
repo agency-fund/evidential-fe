ed05369801a239a54687234f4c745de9b2422603
Linus Wong <ldwong@gmail.com>
2025-03-18 23:48:34 +0800

    remove normCDF

--- src/app/experiments/forest-plot.tsx
+++ src/app/experiments/forest-plot.tsx
@@ -207,14 +207,3 @@ export function ForestPlot({ analysis, armNames, controlArmIndex = 0, experiment
     </Card>
   );
 }
-
-// Standard normal cumulative distribution function
-function normCDF(x: number): number {
-  const t = 1 / (1 + 0.2316419 * Math.abs(x));
-  const d = 0.3989423 * Math.exp((-x * x) / 2);
-  let p = d * t * (0.3193815 + t * (-0.3565638 + t * (1.781478 + t * (-1.821256 + t * 1.330274))));
-  if (x > 0) {
-    p = 1 - p;
-  }
-  return p;
-}
