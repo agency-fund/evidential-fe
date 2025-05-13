1abfef6ac8b33cd50bf9adec44dfacc914d95224
Blake C. Douglass <blakecdouglass@gmail.com>
2025-05-13 10:42:42 -0700

    import cleanup complete

--- src/app/organizations/page.tsx
+++ src/app/organizations/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { Flex, Heading, Text } from '@radix-ui/themes';
-import { XSpinner } from '../components/ui/x-spinner';
+import { XSpinner } from '@/app/components/ui/x-spinner';
 import { useListOrganizations } from '@/api/admin';
 import { CreateOrganizationDialog } from '@/app/components/features/organizations/create-organization-dialog';
 import { OrganizationsTable } from '@/app/components/features/organizations/organizations-table';
-import { EmptyStateCard } from '../components/ui/cards/empty-state-card';
+import { EmptyStateCard } from '@/app/components/ui/cards/empty-state-card';
 export default function Page() {
   const { data, isLoading, error } = useListOrganizations();
 
