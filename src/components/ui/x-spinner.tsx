8115f36b082b03939c81f0d1d17588d896ae148b
Linus Wong <ldwong@gmail.com>
2025-05-15 22:07:01 +0800

    allow size control over our XSpinner.

--- src/components/ui/x-spinner.tsx
+++ src/components/ui/x-spinner.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { Flex, Spinner, Text } from '@radix-ui/themes';
+import { Responsive } from '@radix-ui/themes/props';
 
-export function XSpinner({ message }: { message?: string }) {
+export function XSpinner({ message, size = '2' }: { message?: string; size?: Responsive<'2' | '1' | '3'> }) {
   return (
     <Flex gap="2" align="center">
-      <Spinner />
-      {message && <Text>{message}</Text>}
+      <Spinner size={size} />
+      {message && <Text size={size}>{message}</Text>}
     </Flex>
   );
 }
