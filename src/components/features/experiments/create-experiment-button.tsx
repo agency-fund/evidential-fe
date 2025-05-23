78f2f9a2baaddd6f86c3222a2ea938ffbf0bfc9c
dctaf <155950283+dctaf@users.noreply.github.com>
2025-05-23 15:46:41 -0700

    Fix import of next/navigation router.

--- src/components/features/experiments/create-experiment-button.tsx
+++ src/components/features/experiments/create-experiment-button.tsx
@@ -2,7 +2,7 @@ import { ListDatasourcesResponse } from '@/api/methods.schemas';
 import Link from 'next/link';
 import { Button, DropdownMenu } from '@radix-ui/themes';
 import { PlusIcon } from '@radix-ui/react-icons';
-import { router } from 'next/client';
+import { useRouter } from 'next/navigation';
 
 export function CreateExperimentButton({
   loading,
@@ -11,6 +11,7 @@ export function CreateExperimentButton({
   loading: boolean;
   datasources: ListDatasourcesResponse | undefined;
 }) {
+  const router = useRouter();
   if (!loading && datasources?.items.length === 1) {
     return (
       <Link href={`/datasources/${datasources.items[0].id}/experiments/create`}>
