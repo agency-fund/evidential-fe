fd052f9ef02c41c36b32db6fd5571918f7fdd908
dctaf <155950283+dctaf@users.noreply.github.com>
2025-07-04 14:59:24 -0700

    Use environment variables to specify support email and API docs link.

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -4,5 +4,5 @@ export const OIDC_BASE_URL = process.env.NEXT_PUBLIC_XNGIN_OIDC_BASE_URL;
 export const OIDC_CLIENT_ID = process.env.NEXT_PUBLIC_XNGIN_GOOGLE_CLIENT_ID;
 export const OIDC_REDIRECT_URI = process.env.NEXT_PUBLIC_XNGIN_OIDC_REDIRECT_URI;
 export const PRODUCT_NAME = 'Evidential';
-export const SUPPORT_EMAIL = 'evidential-support@agency.fund';
-export const XNGIN_API_DOCS_LINK = process.env.NEXT_PUBLIC_XNGIN_API_DOCS ?? 'https://main.dev.agencyfund.org/docs';
+export const SUPPORT_EMAIL = process.env.NEXT_PUBLIC_XNGIN_SUPPORT_EMAIL ?? 'support@example.com';
+export const XNGIN_API_DOCS_LINK = process.env.NEXT_PUBLIC_XNGIN_API_DOCS ?? 'https://example.com/docs';
