a5872af9d3fe8e10e1de44748a886dc0d7d4f581
dctaf <155950283+dctaf@users.noreply.github.com>
2025-08-10 21:34:32 -0700

    genapiclient, remove obsolete code blocks, and fix bug breaking postgres datasource edits. (#122)
    * task genapiclient
    
    * Remove obsolete conditionals relating to sheet support.
    
    * Restore psycopg3 support (bug introduced by df2ddb639c7d3faebf4ad3f949263bccb464fd04).

--- src/components/features/datasources/edit-datasource-dialog.tsx
+++ src/components/features/datasources/edit-datasource-dialog.tsx
@@ -82,7 +82,7 @@ export const EditDatasourceDialog = ({
           setProjectId(dwh.project_id);
           setDataset(dwh.dataset_id);
           setCredentialsJson(atob(dwh.credentials.content_base64));
-        } else if (dwh.driver === 'postgresql+psycopg2') {
+        } else if (dwh.driver === 'postgresql+psycopg2' || dwh.driver === 'postgresql+psycopg') {
           setHost(dwh.host);
           setPort(dwh.port ? dwh.port.toString() : '5432');
           setDbname(dwh.dbname);
