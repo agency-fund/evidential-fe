811d8dc733d991473e2ece5c8de4c5a4a7d99fe3
poornimaramesh <poornimaramesh1995@gmail.com>
2025-08-12 23:52:45 +0530

    update experiment badges

--- src/components/features/experiments/experiment-type-badge.tsx
+++ src/components/features/experiments/experiment-type-badge.tsx
@@ -3,9 +3,10 @@
 import { Badge } from '@radix-ui/themes';
 
 export const ExperimentTypeBadge = ({ type }: { type: string }) => {
-  const typeMap: Record<string, { name: string; color: 'green' | 'blue'; variant?: 'soft' | 'outline' }> = {
-    freq_preassigned: { name: 'Preassigned', color: 'blue', variant: 'soft' },
-    freq_online: { name: 'Online', color: 'green', variant: 'soft' },
+  const typeMap: Record<string, { name: string; color: 'green' | 'blue' | 'red'; variant?: 'soft' | 'outline' }> = {
+    freq_preassigned: { name: 'Traditional A/B: Preassigned', color: 'blue', variant: 'soft' },
+    freq_online: { name: 'Traditional A/B: Online', color: 'green', variant: 'soft' },
+    mab_online: { name: 'Multi-Armed Bandit', color: 'red', variant: 'soft' },
   };
 
   const { name, color, variant } = typeMap[type] || { name: type, color: 'red' as const, variant: 'outline' as const };
