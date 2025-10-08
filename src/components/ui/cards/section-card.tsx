c49b3394d318d75d57b34e69678dd6a16916835f
qixotic <854794+qixotic@users.noreply.github.com>
2025-10-08 15:22:45 +0800

    SectionCard change to take a headerLeft component

--- src/components/ui/cards/section-card.tsx
+++ src/components/ui/cards/section-card.tsx
@@ -3,18 +3,29 @@ import { Card, Flex, Heading, Separator } from '@radix-ui/themes';
 import React from 'react';
 
 export type SectionCardProps = {
-  title: string;
+  /**
+   * Title to display in the header. Optional.
+   * Note: If `headerLeft` is supplied, this prop is ignored.
+   */
+  title?: string;
+  /**
+   * Custom component to display on the left side of the header.
+   * If not provided, defaults to a Heading with the `title` prop.
+   */
+  headerLeft?: React.ReactNode;
   headerRight?: React.ReactNode;
   children: React.ReactNode;
 };
 
-export const SectionCard: React.FC<SectionCardProps> = ({ title, headerRight, children }) => {
+export const SectionCard: React.FC<SectionCardProps> = ({ title, headerLeft, headerRight, children }) => {
   return (
     <Card>
-      <Flex direction="row" gap="4" align="center" justify="between" py="1">
-        <Heading size="3" wrap="nowrap">
-          {title}
-        </Heading>
+      <Flex direction="row" gap="4" align="center" justify="between" py="1" wrap="wrap">
+        {headerLeft ?? (
+          <Heading size="3" wrap="nowrap">
+            {title}
+          </Heading>
+        )}
         {headerRight}
       </Flex>
       <Separator my="2" size="4" />
