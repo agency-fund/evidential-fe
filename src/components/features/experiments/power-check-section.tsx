b1e896a5e170fe4d64657ef50d34939f9a04e164
poornimaramesh <poornimaramesh1995@gmail.com>
2026-01-12 13:23:27 +0300

    update power check component to clarify sample size selection

--- src/components/features/experiments/power-check-section.tsx
+++ src/components/features/experiments/power-check-section.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import { Button, Callout, Card, Flex, Spinner, Table, Text, TextField } from '@radix-ui/themes';
+import { Button, Callout, Card, Flex, Spinner, Table, Text, TextField, RadioGroup } from '@radix-ui/themes';
 import { CheckCircledIcon, CrossCircledIcon, LightningBoltIcon } from '@radix-ui/react-icons';
 import { FrequentABFormData } from '@/app/datasources/[datasourceId]/experiments/create/types';
 import { usePowerCheck } from '@/api/admin';
 import { convertFormDataToCreateExperimentRequest } from '@/app/datasources/[datasourceId]/experiments/create/helpers';
 import { GenericErrorCallout } from '@/components/ui/generic-error';
-import { ZodError } from 'zod';
+import { set, ZodError } from 'zod';
 import { useState } from 'react';
 import { prettyJSON } from '@/services/json-utils';
+import { SectionCard } from '@/components/ui/cards/section-card';
 
 interface PowerCheckSectionProps {
   formData: FrequentABFormData;
@@ -21,6 +22,11 @@ export function PowerCheckSection({ formData, onFormDataChange }: PowerCheckSect
 
   const isButtonDisabled = isMutating || formData.primaryMetric === undefined;
 
+  const [powerCheckTarget, setPowerCheckTarget] = useState<number | undefined>(undefined);
+  const [nonNullSamples, setNonNullSamples] = useState<number | undefined>(undefined);
+  const [allSamples, setAllSamples] = useState<number | undefined>(undefined);
+  const [selectedSampleOption, setSelectedSampleOption] = useState<string>('use_power_check');
+
   const handlePowerCheck = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
 
@@ -28,10 +34,31 @@ export function PowerCheckSection({ formData, onFormDataChange }: PowerCheckSect
     try {
       const { design_spec } = convertFormDataToCreateExperimentRequest(formData);
       const response = await trigger({ design_spec });
+
+      setPowerCheckTarget(response.analyses[0].target_n ?? undefined);
+      const samples =
+        response.analyses
+          .map((a) =>
+            a.metric_spec.field_name == formData.primaryMetric?.metric.field_name
+              ? (a.metric_spec.available_nonnull_n ?? 0)
+              : 0,
+          )
+          .reduce((a, b) => a + b, 0) ?? null;
+      setNonNullSamples(samples);
+      setAllSamples(
+        response.analyses
+          .map((a) =>
+            a.metric_spec.field_name == formData.primaryMetric?.metric.field_name
+              ? (a.metric_spec.available_n ?? 0)
+              : 0,
+          )
+          .reduce((a, b) => a + b, 0) ?? null,
+      );
+
       onFormDataChange({
         ...formData,
         powerCheckResponse: response,
-        chosenN: response.analyses[0].target_n!,
+        chosenN: undefined,
       });
     } catch (err) {
       if (err instanceof ZodError) {
@@ -43,112 +70,163 @@ export function PowerCheckSection({ formData, onFormDataChange }: PowerCheckSect
   };
 
   return (
-    <Flex direction="column" gap="3" align="center">
-      <Button disabled={isButtonDisabled} onClick={handlePowerCheck} style={{ minWidth: '25%' }} loading={isMutating}>
-        <>
-          <LightningBoltIcon />
-          Run Power Check
-        </>
-      </Button>
-      {formData.powerCheckResponse !== undefined && (
-        <>
-          <Text>Target sample size to distribute across all arms: </Text>
-          <TextField.Root
-            style={{
-              minWidth: '50%',
-              textAlign: 'center',
-            }}
-            size="3"
-            type="number"
-            value={formData.chosenN ?? ''}
-            onChange={(e) =>
-              onFormDataChange({ ...formData, chosenN: e.target.value === '' ? undefined : Number(e.target.value) })
-            }
-            placeholder="Run the Power Check, or type your own desired #."
-          />
-        </>
-      )}
+    <Flex direction="column" gap="4">
+      <SectionCard title="Analysis">
+        <Flex direction="column" gap="3" align="center">
+          <Button
+            disabled={isButtonDisabled}
+            onClick={handlePowerCheck}
+            style={{ minWidth: '25%' }}
+            loading={isMutating}
+          >
+            <>
+              <LightningBoltIcon />
+              Run Power Check
+            </>
+          </Button>
 
-      {error && (
-        <Flex align="center" gap="2">
-          <GenericErrorCallout title={'Power check failed'} message={error ? prettyJSON(error) : 'unknown'} />
-        </Flex>
-      )}
-
-      {validationError && (
-        <Flex align="center" gap="2">
-          <GenericErrorCallout
-            title={'Validation failed'}
-            message={validationError.issues.map((issue) => `${issue.path.join('.')}: ${issue.message}`).join('\n')}
-          />
-        </Flex>
-      )}
-
-      {isMutating && (
-        <Flex align="center" gap="2">
-          <Spinner size="1" />
-          <Text>Analyzing metrics and population data...</Text>
-        </Flex>
-      )}
-
-      {formData.powerCheckResponse !== undefined &&
-        !validationError &&
-        formData.powerCheckResponse.analyses.map((metricAnalysis, i) => (
-          <Card key={i}>
-            <Flex direction="column" gap="3">
-              <Text weight={'bold'}>{metricAnalysis.metric_spec.field_name}</Text>
-              <Callout.Root color={metricAnalysis.sufficient_n ? 'green' : 'red'}>
-                <Callout.Icon>{metricAnalysis.sufficient_n ? <CheckCircledIcon /> : <CrossCircledIcon />}</Callout.Icon>
-                <Callout.Text>
-                  {metricAnalysis.msg?.msg ||
-                    (metricAnalysis.sufficient_n
-                      ? `The experiment has sufficient power.`
-                      : `The experiment does not have sufficient power.`)}
-                </Callout.Text>
-              </Callout.Root>
-              <Table.Root>
-                <Table.Body>
-                  <Table.Row>
-                    <Table.RowHeaderCell>Required Sample Size</Table.RowHeaderCell>
-                    <Table.Cell>{metricAnalysis.target_n || '?'}</Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.RowHeaderCell>Available Samples</Table.RowHeaderCell>
-                    <Table.Cell>
-                      {metricAnalysis.metric_spec.available_n == null ? (
-                        '?'
-                      ) : metricAnalysis.metric_spec.available_n === 0 ||
-                        metricAnalysis.metric_spec.available_n < (metricAnalysis.target_n ?? 0) ? (
-                        <span color="crimson">{metricAnalysis.metric_spec.available_n}</span>
-                      ) : (
-                        metricAnalysis.metric_spec.available_n
-                      )}
-                    </Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.RowHeaderCell>Available non-null Samples</Table.RowHeaderCell>
-                    <Table.Cell>
-                      {metricAnalysis.metric_spec.available_nonnull_n == null ? (
-                        '?'
-                      ) : metricAnalysis.metric_spec.available_nonnull_n === 0 ||
-                        metricAnalysis.metric_spec.available_nonnull_n < (metricAnalysis.target_n ?? 0) ? (
-                        <span color="crimson">{metricAnalysis.metric_spec.available_nonnull_n}</span>
-                      ) : (
-                        metricAnalysis.metric_spec.available_nonnull_n
-                      )}
-                    </Table.Cell>
-                  </Table.Row>
-                  {metricAnalysis.pct_change_possible !== null && metricAnalysis.pct_change_possible !== undefined && (
-                    <Table.Row>
-                      <Table.RowHeaderCell>Minimum Detectable Effect with all available samples</Table.RowHeaderCell>
-                      <Table.Cell>{(metricAnalysis.pct_change_possible * 100).toFixed(4)}%</Table.Cell>
-                    </Table.Row>
-                  )}
-                </Table.Body>
-              </Table.Root>
+          {error && (
+            <Flex align="center" gap="2">
+              <GenericErrorCallout title={'Power check failed'} message={error ? prettyJSON(error) : 'unknown'} />
             </Flex>
-          </Card>
-        ))}
+          )}
+
+          {validationError && (
+            <Flex align="center" gap="2">
+              <GenericErrorCallout
+                title={'Validation failed'}
+                message={validationError.issues.map((issue) => `${issue.path.join('.')}: ${issue.message}`).join('\n')}
+              />
+            </Flex>
+          )}
+
+          {isMutating && (
+            <Flex align="center" gap="2">
+              <Spinner size="1" />
+              <Text>Analyzing metrics and population data...</Text>
+            </Flex>
+          )}
+
+          {formData.powerCheckResponse !== undefined &&
+            !validationError &&
+            formData.powerCheckResponse.analyses.map((metricAnalysis, i) => (
+              <Card key={i}>
+                <Flex direction="column" gap="3">
+                  <Text weight={'bold'}>{metricAnalysis.metric_spec.field_name}</Text>
+                  <Callout.Root color={metricAnalysis.sufficient_n ? 'green' : 'red'}>
+                    <Callout.Icon>
+                      {metricAnalysis.sufficient_n ? <CheckCircledIcon /> : <CrossCircledIcon />}
+                    </Callout.Icon>
+                    <Callout.Text>
+                      {metricAnalysis.msg?.msg ||
+                        (metricAnalysis.sufficient_n
+                          ? `The experiment has sufficient power.`
+                          : `The experiment does not have sufficient power.`)}
+                    </Callout.Text>
+                  </Callout.Root>
+                  <Table.Root>
+                    <Table.Body>
+                      <Table.Row>
+                        <Table.RowHeaderCell>Required Sample Size</Table.RowHeaderCell>
+                        <Table.Cell>{metricAnalysis.target_n || '?'}</Table.Cell>
+                      </Table.Row>
+                      <Table.Row>
+                        <Table.RowHeaderCell>Available Samples</Table.RowHeaderCell>
+                        <Table.Cell>
+                          {metricAnalysis.metric_spec.available_n == null ? (
+                            '?'
+                          ) : metricAnalysis.metric_spec.available_n === 0 ||
+                            metricAnalysis.metric_spec.available_n < (metricAnalysis.target_n ?? 0) ? (
+                            <span color="crimson">{metricAnalysis.metric_spec.available_n}</span>
+                          ) : (
+                            metricAnalysis.metric_spec.available_n
+                          )}
+                        </Table.Cell>
+                      </Table.Row>
+                      <Table.Row>
+                        <Table.RowHeaderCell>Available non-null Samples</Table.RowHeaderCell>
+                        <Table.Cell>
+                          {metricAnalysis.metric_spec.available_nonnull_n == null ? (
+                            '?'
+                          ) : metricAnalysis.metric_spec.available_nonnull_n === 0 ||
+                            metricAnalysis.metric_spec.available_nonnull_n < (metricAnalysis.target_n ?? 0) ? (
+                            <span color="crimson">{metricAnalysis.metric_spec.available_nonnull_n}</span>
+                          ) : (
+                            metricAnalysis.metric_spec.available_nonnull_n
+                          )}
+                        </Table.Cell>
+                      </Table.Row>
+                      {metricAnalysis.pct_change_possible !== null &&
+                        metricAnalysis.pct_change_possible !== undefined && (
+                          <Table.Row>
+                            <Table.RowHeaderCell>
+                              Minimum Detectable Effect with all available samples
+                            </Table.RowHeaderCell>
+                            <Table.Cell>{(metricAnalysis.pct_change_possible * 100).toFixed(4)}%</Table.Cell>
+                          </Table.Row>
+                        )}
+                    </Table.Body>
+                  </Table.Root>
+                </Flex>
+              </Card>
+            ))}
+        </Flex>
+      </SectionCard>
+      <SectionCard title="Select Target Sample Size">
+        <Flex direction="column" gap="3" align="start">
+          <Text>Select target sample size to distribute across all arms:</Text>
+          <Flex direction="column" gap="2" align="center">
+            <RadioGroup.Root
+              value={selectedSampleOption}
+              onValueChange={(value) => {
+                setSelectedSampleOption(value);
+
+                if (value === 'use_power_check') {
+                  onFormDataChange({
+                    ...formData,
+                    chosenN: powerCheckTarget,
+                  });
+                } else if (value === 'use_all_non_null_samples') {
+                  onFormDataChange({
+                    ...formData,
+                    chosenN: nonNullSamples,
+                  });
+                } else if (value === 'enter_own') {
+                  onFormDataChange({ ...formData, chosenN: undefined });
+                }
+              }}
+            >
+              <RadioGroup.Item value="use_power_check" disabled={powerCheckTarget === undefined}>
+                Use power check result: {powerCheckTarget ?? 'N/A'}
+              </RadioGroup.Item>
+              <RadioGroup.Item value="use_all_non_null_samples" disabled={nonNullSamples === undefined}>
+                Use all available non-null samples: {nonNullSamples}
+              </RadioGroup.Item>
+              <RadioGroup.Item value="enter_own" disabled={allSamples === undefined}>
+                <Flex align="start" direction={'row'} gap="2">
+                  Use custom sample size:
+                  <TextField.Root
+                    style={{
+                      minWidth: '10%',
+                      textAlign: 'start',
+                    }}
+                    size="2"
+                    type="number"
+                    max={allSamples ?? undefined}
+                    onChange={(e) =>
+                      onFormDataChange({
+                        ...formData,
+                        chosenN: e.target.value === '' ? undefined : Number(e.target.value),
+                      })
+                    }
+                    placeholder="Type your own desired #."
+                  />
+                </Flex>
+              </RadioGroup.Item>
+            </RadioGroup.Root>
+          </Flex>
+        </Flex>
+      </SectionCard>
     </Flex>
   );
 }
