df5e7961dcfc7f62e56f579f24f815b114e1badb
dctaf <155950283+dctaf@users.noreply.github.com>
2026-01-27 19:48:19 -0800

    Upgrade Radix Themes and remove unused dependency. (#211)
    * bump radix to 3.2.1
    
    * remove dependency on toast library.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import OurSwrConfig from '@/providers/our-swr-config';
 import { Suspense } from 'react';
 import { NavigationBarNoCompact } from '@/components/layout/nav/navigation-curtain-no-compact';
 import { HeaderBar } from '@/components/layout/header/header-bar';
-import * as Toast from '@radix-ui/react-toast';
 import RequireLogin from '@/components/require-login';
 
 const geistSans = Geist({
@@ -31,47 +30,34 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`} style={{ margin: 0, padding: 0 }}>
         <Theme appearance="light" accentColor="indigo" grayColor="slate" radius="large">
-          <Toast.Provider swipeDirection="right">
-            <Suspense>
-              <GoogleAuthProvider>
-                <OurSwrConfig>
-                  <RequireLogin>
-                    <OrganizationProvider>
-                      <Flex direction="column" height="100vh">
-                        <HeaderBar />
-                        <Flex flexGrow="1" overflow="hidden">
-                          {/*Temporary always open navigation curtain to experiments and settings nav links are always visible, please do not remove*/}
-                          <NavigationBarNoCompact />
-                          <Flex direction="column" flexGrow="1" overflowY="auto" position="relative">
-                            <Container
-                              py="8"
-                              px="4"
-                              flexGrow="1"
-                              width="100%"
-                              style={{ backgroundColor: 'var(--gray-2)', borderLeft: '1px solid var(--gray-4)' }}
-                            >
-                              <Suspense>{children}</Suspense>
-                            </Container>
-                          </Flex>
+          <Suspense>
+            <GoogleAuthProvider>
+              <OurSwrConfig>
+                <RequireLogin>
+                  <OrganizationProvider>
+                    <Flex direction="column" height="100vh">
+                      <HeaderBar />
+                      <Flex flexGrow="1" overflow="hidden">
+                        {/*Temporary always open navigation curtain to experiments and settings nav links are always visible, please do not remove*/}
+                        <NavigationBarNoCompact />
+                        <Flex direction="column" flexGrow="1" overflowY="auto" position="relative">
+                          <Container
+                            py="8"
+                            px="4"
+                            flexGrow="1"
+                            width="100%"
+                            style={{ backgroundColor: 'var(--gray-2)', borderLeft: '1px solid var(--gray-4)' }}
+                          >
+                            <Suspense>{children}</Suspense>
+                          </Container>
                         </Flex>
                       </Flex>
-                      <Toast.Viewport
-                        style={{
-                          position: 'fixed',
-                          bottom: '20px',
-                          right: '20px',
-                          zIndex: 1000,
-                          listStyle: 'none',
-                          padding: 0,
-                          margin: 0,
-                        }}
-                      />
-                    </OrganizationProvider>
-                  </RequireLogin>
-                </OurSwrConfig>
-              </GoogleAuthProvider>
-            </Suspense>
-          </Toast.Provider>
+                    </Flex>
+                  </OrganizationProvider>
+                </RequireLogin>
+              </OurSwrConfig>
+            </GoogleAuthProvider>
+          </Suspense>
         </Theme>
       </body>
     </html>
