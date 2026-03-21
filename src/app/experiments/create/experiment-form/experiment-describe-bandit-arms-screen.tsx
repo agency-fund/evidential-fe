09952fbd933d776e1330eb10f14690659cea7de2
poornimaramesh <poornimaramesh1995@gmail.com>
2026-03-21 15:51:21 +0300

    update/debug frontend dialog for editing weights

--- src/app/experiments/create/experiment-form/experiment-describe-bandit-arms-screen.tsx
+++ src/app/experiments/create/experiment-form/experiment-describe-bandit-arms-screen.tsx
@@ -2,9 +2,21 @@
 
 import { ScreenProps } from '@/services/wizard/wizard-types';
 import { ExperimentFormData, ExperimentScreenId } from '@/app/experiments/create/experiment-form/experiment-form-def';
-import { Box, Button, Callout, Card, Flex, Heading, IconButton, Text, TextArea, TextField } from '@radix-ui/themes';
+import {
+  Box,
+  Button,
+  Callout,
+  Card,
+  Flex,
+  Heading,
+  IconButton,
+  Text,
+  TextArea,
+  TextField,
+  SegmentedControl,
+} from '@radix-ui/themes';
 import { NavigationButtons } from '@/components/features/experiments/navigation-buttons';
-import { InfoCircledIcon, PlusIcon, TrashIcon } from '@radix-ui/react-icons';
+import { InfoCircledIcon, Pencil2Icon, PlusIcon, TrashIcon } from '@radix-ui/react-icons';
 import { useCreateExperiment, useListOrganizationDatasources } from '@/api/admin';
 import { useCurrentOrganization } from '@/providers/organization-provider';
 import { convertToBanditCreateRequest } from '@/app/experiments/create/experiment-form/experiment-form-helpers';
@@ -14,6 +26,7 @@ import { GenericErrorCallout } from '@/components/ui/generic-error';
 import { XSpinner } from '@/components/ui/x-spinner';
 import { BanditArm, PriorType } from '@/app/experiments/create/experiment-form/experiment-form-types';
 import { ArmWeightsDialog } from '@/components/features/experiments/arm-weights-dialog';
+import { useState } from 'react';
 
 export type ExperimentDescribeBanditArmsMessage =
   | { type: 'add-arm' }
@@ -28,12 +41,13 @@ interface ArmCardProps {
   arm: BanditArm;
   armIndex: number;
   priorType: PriorType;
+  showPriors: boolean;
   canDelete: boolean;
   onUpdate: (field: keyof BanditArm, value: string | number) => void;
   onDelete: () => void;
 }
 
-function ArmCard({ arm, armIndex, priorType, canDelete, onUpdate, onDelete }: ArmCardProps) {
+function ArmCard({ arm, armIndex, priorType, showPriors, canDelete, onUpdate, onDelete }: ArmCardProps) {
   return (
     <Card>
       <Flex direction="column" gap="2">
@@ -73,82 +87,84 @@ function ArmCard({ arm, armIndex, priorType, canDelete, onUpdate, onDelete }: Ar
           />
         </Flex>
 
-        {/* <Flex direction="column" gap="4">
-          {priorType === 'beta' && (
-            <Flex gap="3">
-              <Box>
-                <Text as="label" size="2" weight="bold" mb="6px">
-                  Alpha Prior
-                </Text>
-                <TextField.Root
-                  type="number"
-                  min="1"
-                  value={arm.alpha_prior?.toString() || '1'}
-                  onChange={(e) => onUpdate('alpha_prior', Number(e.target.value))}
-                  placeholder="Prior successes"
-                />
-              </Box>
-              <Box>
-                <Text as="label" size="2" weight="bold" mb="6px">
-                  Beta Prior
-                </Text>
-                <TextField.Root
-                  type="number"
-                  min="1"
-                  value={arm.beta_prior?.toString() || '1'}
-                  onChange={(e) => onUpdate('beta_prior', Number(e.target.value))}
-                  placeholder="Prior failures"
-                />
-              </Box>
-            </Flex>
-          )}
+        {showPriors && (
+          <Flex direction="column" gap="4">
+            {priorType === 'beta' && (
+              <Flex gap="3">
+                <Box>
+                  <Text as="label" size="2" weight="bold" mb="6px">
+                    Prior successes (alpha)
+                  </Text>
+                  <TextField.Root
+                    type="number"
+                    min="1"
+                    value={arm.alpha_prior ? arm.alpha_prior.toString() : '1'}
+                    onChange={(e) => onUpdate('alpha_prior', Number(e.target.value))}
+                    placeholder="1"
+                  />
+                </Box>
+                <Box>
+                  <Text as="label" size="2" weight="bold" mb="6px">
+                    Prior failures (beta)
+                  </Text>
+                  <TextField.Root
+                    type="number"
+                    min="1"
+                    value={arm.beta_prior ? arm.beta_prior.toString() : '1'}
+                    onChange={(e) => onUpdate('beta_prior', Number(e.target.value))}
+                    placeholder="1"
+                  />
+                </Box>
+              </Flex>
+            )}
 
-          {priorType === 'normal' && (
-            <Flex gap="3">
-              <Box maxWidth={'30%'}>
-                <Text as="label" size="2" weight="bold" mb="6px">
-                  Prior Mean
-                </Text>
-                <TextField.Root
-                  type="number"
-                  value={arm.mean_prior !== undefined ? String(arm.mean_prior) : '0'}
-                  onChange={(e) => onUpdate('mean_prior', e.target.value ? Number(e.target.value) : 0)}
-                  placeholder="Prior Mean"
-                />
-              </Box>
-              <Box maxWidth={'30%'}>
-                <Text as="label" size="2" weight="bold" mb="6px">
-                  Prior Standard Deviation
-                </Text>
-                <TextField.Root
-                  type="number"
-                  value={arm.stddev_prior?.toString() || '1'}
-                  onChange={(e) => onUpdate('stddev_prior', Number(e.target.value))}
-                  placeholder="Prior Standard Deviation"
-                />
-              </Box>
-            </Flex>
-          )}
-        </Flex> */}
+            {priorType === 'normal' && (
+              <Flex gap="3">
+                <Box maxWidth={'30%'}>
+                  <Text as="label" size="2" weight="bold" mb="6px">
+                    Prior Mean
+                  </Text>
+                  <TextField.Root
+                    type="number"
+                    value={arm.mean_prior ? arm.mean_prior.toString() : '0'}
+                    onChange={(e) => onUpdate('mean_prior', e.target.value ? Number(e.target.value) : 0)}
+                    placeholder="0"
+                  />
+                </Box>
+                <Box maxWidth={'30%'}>
+                  <Text as="label" size="2" weight="bold" mb="6px">
+                    Prior Standard Deviation
+                  </Text>
+                  <TextField.Root
+                    type="number"
+                    value={arm.stddev_prior ? arm.stddev_prior.toString() : '1'}
+                    onChange={(e) => onUpdate('stddev_prior', e.target.value ? Number(e.target.value) : 1)}
+                    placeholder="1"
+                  />
+                </Box>
+              </Flex>
+            )}
+          </Flex>
+        )}
       </Flex>
     </Card>
   );
 }
 
-// const isPriorParamValid = (arm: BanditArm, priorType: PriorType) => {
-//   if (priorType === 'beta') {
-//     return arm.alpha_prior !== undefined && arm.alpha_prior > 0 && arm.beta_prior !== undefined && arm.beta_prior > 0;
-//   }
-//   if (priorType === 'normal') {
-//     return arm.mean_prior !== undefined && arm.stddev_prior !== undefined && arm.stddev_prior > 0;
-//   }
-//   return false;
-// };
+const isPriorParamValid = (arm: BanditArm, priorType: PriorType) => {
+  if (priorType === 'beta') {
+    return arm.alpha_prior !== undefined && arm.alpha_prior > 0 && arm.beta_prior !== undefined && arm.beta_prior > 0;
+  }
+  if (priorType === 'normal') {
+    return arm.mean_prior !== undefined && arm.stddev_prior !== undefined && arm.stddev_prior > 0;
+  }
+  return false;
+};
 
-const getValidationMessage = (data: ExperimentFormData) => {
+const getValidationMessage = (data: ExperimentFormData, showPriors: boolean) => {
   if (data.bandit === undefined) return 'Bandit configuration is required.';
   const arms = data.bandit.arms;
-  // const priorType = data.bandit.priorType;
+  const priorType = data.bandit.priorType;
 
   if (arms.length < 2) return 'At least 2 arms are required.';
   if (arms.length > 10) return 'Maximum 10 arms allowed.';
@@ -156,12 +172,12 @@ const getValidationMessage = (data: ExperimentFormData) => {
   for (let i = 0; i < arms.length; i++) {
     const arm = arms[i];
     if (!arm.arm_name?.trim()) return `Arm ${i + 1} name is required.`;
-    // if (!isPriorParamValid(arm, priorType)) return `Arm ${i + 1} has invalid prior parameters.`;
+    if (showPriors && !isPriorParamValid(arm, priorType)) return `Arm ${i + 1} has invalid prior parameters.`;
   }
   return '';
 };
 
-const isFormValid = (data: ExperimentFormData) => getValidationMessage(data) === '';
+const isFormValid = (data: ExperimentFormData, showPriors: boolean) => getValidationMessage(data, showPriors) === '';
 
 export const ExperimentDescribeBanditArmsScreen = ({
   data,
@@ -205,21 +221,45 @@ export const ExperimentDescribeBanditArmsScreen = ({
     },
   );
 
+  const [showPriors, setShowPriors] = useState(false);
+
   const arms = data.bandit?.arms ?? [];
   const priorType = data.bandit?.priorType ?? 'normal';
   const outcomeType = data.bandit?.outcomeType;
-  const formValid = isFormValid(data);
+  const formValid = isFormValid(data, showPriors);
 
   const handleCreate = async () => {
     if (!datasourceId) {
       console.error('No NoDWH datasource found');
       return;
     }
-    if (!isFormValid(data)) {
+    if (!isFormValid(data, showPriors)) {
       return;
     }
     const request = convertToBanditCreateRequest(data);
     await triggerCreate(request, { throwOnError: false });
+  };
+
+  const handleModeChange = (value: string) => {
+    if (value === 'priors') {
+      setShowPriors(true);
+      dispatch({ type: 'set-weights', weights: [] });
+      for (let i = 0; i < arms.length; i++) {
+        dispatch({ type: 'update-arm', index: i, field: 'alpha_prior', value: 1 });
+        dispatch({ type: 'update-arm', index: i, field: 'beta_prior', value: 1 });
+        dispatch({ type: 'update-arm', index: i, field: 'mean_prior', value: 0 });
+        dispatch({ type: 'update-arm', index: i, field: 'stddev_prior', value: 1 });
+      }
+    } else if (value === 'weights') {
+      setShowPriors(false);
+      dispatch({ type: 'set-weights', weights: [50, 50] });
+      for (let i = 0; i < arms.length; i++) {
+        dispatch({ type: 'update-arm', index: i, field: 'alpha_prior', value: '' });
+        dispatch({ type: 'update-arm', index: i, field: 'beta_prior', value: '' });
+        dispatch({ type: 'update-arm', index: i, field: 'mean_prior', value: '' });
+        dispatch({ type: 'update-arm', index: i, field: 'stddev_prior', value: '' });
+      }
+    }
   };
 
   if (datasourcesLoading) {
@@ -243,12 +283,6 @@ export const ExperimentDescribeBanditArmsScreen = ({
   return (
     <>
       <Flex direction="column" gap={'3'}>
-        {/* <Heading as="h3" size={'3'}>
-          Define Treatment Arms
-        </Heading>
-        <Text size="2" color="gray">
-          Define the treatment arms for your experiment. Each arm can have its own prior parameters.
-        </Text> */}
         <Flex justify="between" align="start">
           <Flex direction="column" gap="1">
             <Heading size="4">Arms</Heading>
@@ -258,7 +292,27 @@ export const ExperimentDescribeBanditArmsScreen = ({
               </Text>
             )}
           </Flex>
-          <ArmWeightsDialog arms={arms} onWeightsChange={(weights) => dispatch({ type: 'set-weights', weights })} />
+          <Flex gap="2">
+            <SegmentedControl.Root
+              size="1"
+              variant="surface"
+              value={showPriors ? 'priors' : 'weights'}
+              onValueChange={(value) => handleModeChange(value)}
+            >
+              <SegmentedControl.Item value="priors">Edit Priors</SegmentedControl.Item>
+              <SegmentedControl.Item value="weights">
+                <Flex gap="1" align="center">
+                  Edit Weights
+                </Flex>
+              </SegmentedControl.Item>
+            </SegmentedControl.Root>
+            <ArmWeightsDialog
+              arms={arms}
+              onWeightsChange={(weights) => dispatch({ type: 'set-weights', weights })}
+              disabled={showPriors}
+              button_text=""
+            />
+          </Flex>
         </Flex>
 
         {/* Prior configuration info */}
@@ -279,6 +333,7 @@ export const ExperimentDescribeBanditArmsScreen = ({
               arm={arm}
               armIndex={index}
               priorType={priorType}
+              showPriors={showPriors}
               canDelete={arms.length > 2}
               onUpdate={(field, value) => dispatch({ type: 'update-arm', index, field, value })}
               onDelete={() => dispatch({ type: 'remove-arm', index })}
@@ -304,7 +359,7 @@ export const ExperimentDescribeBanditArmsScreen = ({
         onNext={handleCreate}
         nextDisabled={!formValid}
         nextLoading={createLoading}
-        nextTooltipContent={getValidationMessage(data)}
+        nextTooltipContent={getValidationMessage(data, showPriors)}
       />
     </>
   );
