5de7ee90d10793fbdabdb7c7ea1ce01b46353401
dctaf <155950283+dctaf@users.noreply.github.com>
2026-05-22 22:08:41 -0700

    Adjust component hierarchy of resend-webhook-dialog + tooltips.

--- src/components/features/organizations/resend-webhook-dialog.tsx
+++ src/components/features/organizations/resend-webhook-dialog.tsx
@@ -21,18 +21,18 @@ export function ResendWebhookDialog({ organizationId, eventId, open, onOpenChang
 
   return (
     <AlertDialog.Root open={open} onOpenChange={onOpenChange}>
-      <AlertDialog.Trigger>
-        <Tooltip content="Resend">
+      <Tooltip content="Resend">
+        <AlertDialog.Trigger>
           <IconButton variant="soft" color="gray">
             <ReloadIcon />
           </IconButton>
-        </Tooltip>
-      </AlertDialog.Trigger>
+        </AlertDialog.Trigger>
+      </Tooltip>
       <AlertDialog.Content>
         <AlertDialog.Title>Resend webhook?</AlertDialog.Title>
         <AlertDialog.Description>
           This will requeue the original webhook request to your server. Your infrastructure may receive multiple events
-          for the same experiment if a previous attempt actually succeeded.
+          for the same experiment.
         </AlertDialog.Description>
         <Flex gap="3" mt="4" justify="end">
           <AlertDialog.Cancel>
