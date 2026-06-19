4cbbbd8ae40cb43924ba58f776d518c7433fe105
poornimaramesh <poornimaramesh1995@gmail.com>
2026-06-19 13:47:11 +0300

    fix linting

--- src/components/features/experiments/querybuilder/bigint-filter-input.tsx
+++ src/components/features/experiments/querybuilder/bigint-filter-input.tsx
@@ -15,7 +15,7 @@ import { z } from 'zod';
 import { IncludeNullButton } from '@/components/features/experiments/querybuilder/include-null-button';
 import { AddValueButton } from '@/components/features/experiments/querybuilder/add-value-button';
 
-export interface BigIntFilterInputProps {
+export interface BigIntFilterProps {
   filter: Filter & TypedFilter<string>;
   onChange: (filter: Filter) => void;
   dataType: DataType;
