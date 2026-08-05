69a8987dba25ea692f5b26477bc017088b1277d4
poornimaramesh <poornimaramesh1995@gmail.com>
2026-08-05 11:50:32 +0300

    add autofail badge to experiment card

--- src/app/datasources/[datasourceId]/experiments/[experimentId]/page.tsx
+++ src/app/datasources/[datasourceId]/experiments/[experimentId]/page.tsx
@@ -502,6 +502,15 @@ export default function ExperimentViewPage() {
                     <Text size="2">{totalClusterCount.toLocaleString()} clusters</Text>
                   </Badge>
                 )}
+                {isBanditSpec(design_spec) && design_spec.enable_autofail && (
+                  <Badge variant="soft">
+                    <InfoCircledIcon />
+                    <Text size="2">
+                      Autofail enabled: (time: {design_spec.autofail_window}h, outcome:{' '}
+                      {design_spec.autofail_outcome_value})
+                    </Text>
+                  </Badge>
+                )}
                 <Badge>
                   <PersonIcon />
                   <Text size="2">{assign_summary.sample_size.toLocaleString()} participants</Text>
